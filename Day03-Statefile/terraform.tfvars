557c0f8e9e38904c4616bdfca6ee12d934e71622
murthychiluka <murthychiluka@gmail.com>
2026-05-17 18:18:00 +0530

    added Day03

---
+++ Day03-Statefile/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id = "ami-0cca150d127c2216f"
+instance_type = "t3.micro"
---
+++ Day03-Statefile/variables.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+    description = "passing the ami value"
+    default = ""
+    type = string
+
+}
+variable "instance_type" {
+    default = ""
+    type = string
+}   
