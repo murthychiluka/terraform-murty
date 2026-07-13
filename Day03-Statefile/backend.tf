4f1fa6bbedcbf6d33a82e12d33830bd88da5b580
murthychiluka <murthychiluka@gmail.com>
2026-07-13 22:05:16 +0530

    added lock file

--- Day03-Statefile/backend.tf
+++ Day03-Statefile/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "murthy-terraform"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    use_lockfile = true
+    use_lockfile = true ##supports terrafrom latest version >=1.10
+    # dynamodb_table = "terraform-state-locking"  #if terrafrom version <1.10 use below code
   }
 }
