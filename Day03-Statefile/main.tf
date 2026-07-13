cfd62591f9795e6ca8cc3e128db679caf2190c43
murthychiluka <murthychiluka@gmail.com>
2026-07-13 23:28:11 +0530

    Day03

--- Day03-Statefile/main.tf
+++ Day03-Statefile/main.tf
@@ -1,13 +1,21 @@
-resource "aws_instance" "name" {
+# resource "aws_instance" "name" {
      
-     ami = var.ami
-     instance_type = var.instance_type
-     tags = {
-       Name = "dev-instance"
-     }
-}
+#      ami = var.ami
+#      instance_type = var.instance_type
+#      tags = {
+#        Name = "dev-instance"
+#      }
+# }
 
+resource "aws_instance" "name" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "dev-instance"
+  }
+}
 
 resource "aws_vpc" "name" {
   cidr_block = "10.0.0.0/16"
- }
+}
