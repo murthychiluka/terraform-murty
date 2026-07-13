16f31e9fb7425022f0758f77edd379f30f02c69e
murthychiluka <murthychiluka@gmail.com>
2026-07-13 20:21:08 +0530

    statefile task

--- Day03-Statefile/backend.tf
+++ Day03-Statefile/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "murthy-terraform"
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "murthy-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
---
+++ Day15-opentofu/main.tf
@@ -0,0 +1,5 @@
+resource "aws_instance" "name" {
+     
+     ami = "ami-0cca150d127c2216f"
+     instance_type = "t3.micro"
+}
